12b5e77fe1171078a076241ca3f0c4e85e0072e5
Quinton Fults <quinton.fults.work@gmail.com>
2021-08-13 13:25:35 -0700

    code

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,4 @@
-let posts = [
-  {
-    title: 'Example Post',
-    body: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead. Cum horribilem walking dead resurgere de crazed sepulcris creaturis, zombie sicut de grave feeding iride et serpens. Pestilentia, shaun ofthe dead scythe animated corpses ipsa screams. Pestilentia est plague.'
-  }
-]
+const posts = JSON.parse(localStorage.getItem('posts')) || []
 
 const renderPosts = () => {
   document.getElementById('posts').innerHTML = ''
@@ -15,6 +10,7 @@ const renderPosts = () => {
     <div class="card-body">
       <h5 class="card-title">${posts[i].title}</h5>
       <p class="card-text">${posts[i].body}</p>
+      <button class="btn btn-danger delete" data-index="${i}">delete</button>
     </div>
   `
     document.getElementById('posts').prepend(postElem)
@@ -29,10 +25,23 @@ document.getElementById('createPost').addEventListener('click', event => {
 
   posts.push({ title, body })
 
+  localStorage.setItem('posts', JSON.stringify(posts))
+
   renderPosts()
 
   document.getElementById('title').value = ''
   document.getElementById('body').value = ''
+})
+
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('delete')) {
+    const index = parseInt(event.target.dataset.index)
+    posts.splice(index, 1)
+
+    localStorage.setItem('posts', JSON.stringify(posts))
+
+    renderPosts()
+  }
 })
 
 renderPosts()
